b02aac97379d24d94c8c3103cf2ead2ccd672d97
tutu <t433567423@163.com>
2023-09-28 19:46:52 +0800

    feat: 样式重置

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+// 导入css重置
+import './assets/css/reset.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App/>
