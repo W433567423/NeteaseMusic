870dc9754cec04a2a7f5bad2ab3b8b7fd1d2737d
tutu <t433567423@163.com>
2023-09-29 10:56:38 +0800

    feat: 路由

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,14 +1,41 @@
 import {memo} from "react";
-import {NavLink} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 const TuAppHeader = memo(() => {
+    const router = useNavigate()
     return (
         <div>
-            TuAppHeader
-            <hr/>
-            <NavLink style={{padding: '4px'}} to="/">discover</NavLink>
-            <NavLink style={{padding: '4px'}} to="/friend">friend</NavLink>
-            <NavLink style={{padding: '4px'}} to="/mine">mine</NavLink>
+            <h2>TuAppHeader</h2>
+            <button style={{
+                cursor: 'pointer',
+                padding: '6px',
+                border: '1px solid #000',
+                borderRadius: '4px',
+                marginRight: '12px'
+            }}
+                    onClick={() => router('/')}>discover
+            </button>
+            <button style={{
+                cursor: 'pointer',
+                padding: '6px',
+                border: '1px solid #000',
+                borderRadius: '4px',
+                marginRight: '12px'
+            }}
+                    onClick={() => router('/friend')}>friend
+            </button>
+            <button style={{
+                cursor: 'pointer',
+                padding: '6px',
+                border: '1px solid #000',
+                borderRadius: '4px',
+                marginRight: '12px'
+            }}
+                    onClick={() => router('/mine')}>mine
+            </button>
+            {/*<NavLink style={{padding: '4px'}} to="/">discover</NavLink>*/}
+            {/*<NavLink style={{padding: '4px'}} to="/friend">friend</NavLink>*/}
+            {/*<NavLink style={{padding: '4px'}} to="/mine">mine</NavLink>*/}
         </div>
     )
 })
