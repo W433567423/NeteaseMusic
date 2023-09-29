e6fadd449c7a1a578e0d35ab0f6b6082469ac507
tutu <t433567423@163.com>
2023-09-29 11:25:41 +0800

    feat: logo

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,42 +1,38 @@
 import {memo} from "react";
-import {useNavigate} from "react-router-dom";
+import {HeaderLeft, HeaderRight, HeaderWrapper} from "@cpns/app-header/style.ts";
 
 const TuAppHeader = memo(() => {
-    const router = useNavigate()
+    // const router = useNavigate()
+    // const buttonCss = {
+    //     cursor: 'pointer',
+    //     padding: '6px',
+    //     border: '1px solid #000',
+    //     borderRadius: '4px',
+    //     marginRight: '12px'
+    // }
+
     return (
-        <div>
-            <h2>TuAppHeader</h2>
-            <button style={{
-                cursor: 'pointer',
-                padding: '6px',
-                border: '1px solid #000',
-                borderRadius: '4px',
-                marginRight: '12px'
-            }}
-                    onClick={() => router('/')}>discover
-            </button>
-            <button style={{
-                cursor: 'pointer',
-                padding: '6px',
-                border: '1px solid #000',
-                borderRadius: '4px',
-                marginRight: '12px'
-            }}
-                    onClick={() => router('/friend')}>friend
-            </button>
-            <button style={{
-                cursor: 'pointer',
-                padding: '6px',
-                border: '1px solid #000',
-                borderRadius: '4px',
-                marginRight: '12px'
-            }}
-                    onClick={() => router('/mine')}>mine
-            </button>
-            {/*<NavLink style={{padding: '4px'}} to="/">discover</NavLink>*/}
-            {/*<NavLink style={{padding: '4px'}} to="/friend">friend</NavLink>*/}
-            {/*<NavLink style={{padding: '4px'}} to="/mine">mine</NavLink>*/}
-        </div>
+        <HeaderWrapper>
+            <div className="content wrap-v1">
+                <HeaderLeft>
+                    <a href="#/" className='logo sprite_01'></a>
+                </HeaderLeft>
+                <HeaderRight>right</HeaderRight>
+                {/*<button style={buttonCss}*/}
+                {/*        onClick={() => router('/')}>discover*/}
+                {/*</button>*/}
+                {/*<button style={buttonCss}*/}
+                {/*        onClick={() => router('/friend')}>friend*/}
+                {/*</button>*/}
+                {/*<button style={buttonCss}*/}
+                {/*        onClick={() => router('/mine')}>mine*/}
+                {/*</button>*/}
+
+                {/*<NavLink style={{padding: '4px'}} to="/">discover</NavLink>*/}
+                {/*<NavLink style={{padding: '4px'}} to="/friend">friend</NavLink>*/}
+                {/*<NavLink style={{padding: '4px'}} to="/mine">mine</NavLink>*/}</div>
+            <div className="divider"></div>
+        </HeaderWrapper>
     )
 })
 
