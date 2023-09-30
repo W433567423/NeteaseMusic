78609566ee3e87aba2f3f5f1e5c29ca8ce22ae40
tutu <t433567423@163.com>
2023-09-30 16:59:48 +0800

    feat: header组件

---
+++ src/common/type.ts
@@ -0,0 +1,17 @@
+interface ILocalRelation {
+    title: string
+    link: string
+}
+
+interface ILocalFooterImg {
+    link: string
+}
+
+interface IHotRadio {
+    picUrl: string
+    name: string
+    position: string
+    url: string
+}
+
+export type {ILocalRelation, ILocalFooterImg, IHotRadio}
--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,21 +1,28 @@
 import {memo} from "react";
 import {HeaderLeft, HeaderRight, HeaderWrapper} from "@cpns/app-header/style.ts";
+import {headerLinks} from "@/common/local-data.ts";
+import {NavLink} from "react-router-dom";
+import {ILocalRelation} from "@/common/type.ts";
 
 const TuAppHeader = memo(() => {
-    // const router = useNavigate()
-    // const buttonCss = {
-    //     cursor: 'pointer',
-    //     padding: '6px',
-    //     border: '1px solid #000',
-    //     borderRadius: '4px',
-    //     marginRight: '12px'
-    // }
+    const showSelectItem = (e: ILocalRelation, i: number) => {
+        if (i < 3)
+            return <NavLink to={e.link}><i className='sprite_01 icon'></i>{e.title}</NavLink>
+        else
+            return <a href={e.link}>{e.title}</a>
+    }
 
     return (
         <HeaderWrapper>
             <div className="content wrap-v1">
                 <HeaderLeft>
                     <a href="#/" className='logo sprite_01'></a>
+
+                    <div className='select-list'>
+                        {headerLinks.map((e, i) => {
+                            return (<div key={e.title} className='select-item'>{showSelectItem(e, i)}</div>)
+                        })}
+                    </div>
                 </HeaderLeft>
                 <HeaderRight>right</HeaderRight>
                 {/*<button style={buttonCss}*/}
