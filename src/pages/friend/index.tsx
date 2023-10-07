926315102ddbb6c12ca0808c162caabadc52b23a
tutu <t433567423@163.com>
2023-10-07 09:13:57 +0800

    feat: friend页面

--- src/pages/friend/index.tsx
+++ src/pages/friend/index.tsx
@@ -1,10 +1,15 @@
 import {memo} from "react";
+import {FriendWrapper} from "@/pages/friend/style.ts";
 
 const TuFriend = memo(() => {
     return (
-        <>
-            <h2 style={{height: "100px", backgroundColor: "green"}}>TuFriend</h2>
-        </>
+        <FriendWrapper>
+            <div className="content wrap-v2">
+                <div className="pic">
+                    <a className="login" href="/#">立即登录</a>
+                </div>
+            </div>
+        </FriendWrapper>
     )
 })
 
