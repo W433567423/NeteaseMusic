84bd2c5cd7f35f2ee862a6fe9133cea6bb491dae
tutu <t433567423@163.com>
2023-10-07 14:39:08 +0800

    feat: axios尝试网络请求

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
@@ -6,8 +5,8 @@ import App from './App.tsx'
 import '@/assets/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
+    // <React.StrictMode>
+    <App/>
+    // </React.StrictMode>,
 )
 
