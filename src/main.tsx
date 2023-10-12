ff98fe70ce96348b5b1708633e59b1b081a4d7b2
tutu <t433567423@163.com>
2023-10-12 23:32:57 +0800

    feat: 提交缓冲区文件

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from './App.tsx'
 // 导入css重置
 import '@/assets/css/index.css'
 
+import {Map} from "immutable";
+
+Map()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     // <React.StrictMode>
     <App/>
