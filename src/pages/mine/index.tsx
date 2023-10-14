166f4a90ba682fa96f9abae39a59065e4ff9f011
tutu <t433567423@163.com>
2023-10-14 11:44:25 +0800

    feat: mine页面

--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -1,10 +1,15 @@
 import {memo} from "react";
+import {MineWrapper} from "@/pages/mine/style.ts";
 
 const TuMine = memo(() => {
     return (
-        <>
-            <h2 style={{height: "100px", backgroundColor: "yellow"}}>TuMine</h2>
-        </>
+        <MineWrapper>
+            <div className="content wrap-v2">
+                <div className="pic">
+                    <a className="login" href="/#">立即登录</a>
+                </div>
+            </div>
+        </MineWrapper>
     )
 })
 
